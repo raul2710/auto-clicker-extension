7d902ca03bbd2ed6fa00f8955104982d6540a80e
raul2710 <raultavares27@outlook.com>
2024-08-09 19:42:51 -0300

    last update

--- content.js
+++ content.js
@@ -1,23 +1,64 @@
 var clickInterval = null;
-const refreshHivemindButton = document.querySelector('btn btn-link m-0 mr-1 p-0 btn-primary btn-sm');
+var refreshHivemindButton = null;
+var listRefreshHivemindButton = null;
+var intTimedelay = 5;
 
-function startClicker(){
-    clickInterval = setInterval(()=>{refreshHivemindButton.click()}, 1000);
+console.log('\n\n\nStart...\n\n\n');
+
+function startClicker(timeDelay){
+    
+    intTimedelay = parseInt(timeDelay);
+
+    listRefreshHivemindButton = document.querySelectorAll('.widget-header.pr-2.pl-2.m-0>span>b');
+
+    console.log(refreshHivemindButton);
+
+    listRefreshHivemindButton.forEach(element => {
+        if (element.textContent === 'Hivemind Feedback') {
+            refreshHivemindButton = element.parentElement.parentElement.querySelector('.fa.fa-refresh.text-primary').parentElement;
+        }
+    });
+
+    clickInterval = setInterval(()=>{
+        listRefreshHivemindButton = document.querySelectorAll('.widget-header.pr-2.pl-2.m-0>span>b');
+    
+        console.log(refreshHivemindButton);
+    
+        listRefreshHivemindButton.forEach(element => {
+            if (element.textContent === 'Hivemind Feedback') {
+                refreshHivemindButton = element.parentElement.parentElement.querySelector('.fa.fa-refresh.text-primary').parentElement;
+            }
+        });
+        
+        refreshHivemindButton.click();
+    }, intTimedelay*1000);
 }
 
 function stopClicker(){
     clearInterval(clickInterval);
+    clickInterval = null;
 }
 
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-
+        console.log(request.delayTime);
         if (request.action === "startClick"){
-            startClicker();
+            console.log("Start beep");
+            startClicker(request.delayTime);
         }
         else if (request.action === "stopClick") {
-            console.log("Start beep");
+            console.log("Stop beep");
             stopClicker();
         }
+        if (request.action === "isStart") {
+            if (clickInterval) {
+                console.log('true');
+                sendResponse({isStart:"true"});
+            }
+            else {
+                console.log('false');
+                sendResponse({isStart:"false"});
+            }
+        } 
     }
 );
--- popup.js
+++ popup.js
@@ -7,59 +7,29 @@ var isStart = false;
 var counter = 0;
 var counterInterval = null;
 
-
 const [tab] = await chrome.tabs.query({
-    url: "https://console.sara.synkar.com/#/c3/robot?robot*",
+    // url: "https://console.sara.synkar.com/#/c3/robot?robot*",
     active: true,
     currentWindow: true
 })
 
-// function stopCounter(){
-//     clearInterval(counterInterval);
-//     counterInterval = null;
-// }
+const response = await chrome.tabs.sendMessage(tab.id, {action: 'isStart'});
 
-// https://console.sara.synkar.com/#/c3/robot?robot*
-
-// // const clickSimulate = ()=>{
-// //     showMinute.textContent = counter;
-// //     counter++;
-// // }
-// function testeStart(){
-//     itemClickable = document.getElementById('addbtn');
-    
-//     clickInterval = setInterval(() => {
-//         itemClickable.click();
-//     }, 1000);
-// }
-
-// function testeStop(){
-//     itemClickable = document.getElementById('addbtn');
-    
-//     chrome.scripting.executeScript({
-//         target: {tabId: tab.id},
-//         function: ()=>{
-//             clearInterval(clickInterval);
-//         }
-//     })
-
-// }
-
-// function startCounter(){
-//     counter = 0;
-//     showMinute.textContent = counter;
-//     // counterInterval = setInterval(clickSimulate, inputSec.value*1000);
-
-//     chrome.scripting.executeScript({
-//         target: {tabId: tab.id},
-//         function: testeStart
-//     })
-// }
-
-
+if (response.isStart === "true") {
+    console.log('true');
+    isStart = true;
+    btnInit.textContent = 'Stop';
+    btnInit.classList.add('btnstart-stop');
+}
+  
 btnInit.addEventListener('click', async ()=>{
     if (!isStart) {
-        await chrome.tabs.sendMessage(tab.id, { action: 'startClick' });
+        counterInterval = inputSec.value;
+        console.log(counterInterval);
+        await chrome.tabs.sendMessage(tab.id, { 
+            action: 'startClick',
+            delayTime: counterInterval
+        });
 
         isStart = true;
         btnInit.textContent = 'Stop';
